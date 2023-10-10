99e69f5f8522405373a6b8db5bc6aa21b59bdace
Adrian Herrera <adrianhlinares@gmail.com>
2023-10-10 08:17:34 -0400

    fetch de dato

--- pages/administracion/cajas/crear.tsx
+++ pages/administracion/cajas/crear.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
-
-import { Input, HelperText, Label, Select, Textarea } from '@roketid/windmill-react-ui'
-import CTA from 'example/components/CTA'
-import PageTitle from 'example/components/Typography/PageTitle'
-import SectionTitle from 'example/components/Typography/SectionTitle'
-
-import Layout from 'example/containers/Layout'
-import { MailIcon } from 'icons'
+import React from "react";
+import { Input, Label } from "@roketid/windmill-react-ui";
+import PageTitle from "example/components/Typography/PageTitle";
+import Layout from "example/containers/Layout";
 
 function CrearServicio() {
   return (
@@ -15,26 +10,35 @@ function CrearServicio() {
       <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
         <Label>
           <span>Nombre del servicio</span>
-          <Input className="mt-1" placeholder='Ingresa el nombre del servicio'/>
+          <Input
+            className="mt-1"
+            placeholder="Ingresa el nombre del servicio"
+          />
         </Label>
         <Label className="mt-4">
           <span>Ubicación</span>
-          <Input className="mt-1" placeholder='Ingresa la ubicación del servicio'/>
+          <Input
+            className="mt-1"
+            placeholder="Ingresa la ubicación del servicio"
+          />
         </Label>
         <Label className="mt-4">
           <span>Encargado</span>
-          <Input className="mt-1" placeholder='Ingresa el encargado del servicio'/>
+          <Input
+            className="mt-1"
+            placeholder="Ingresa el encargado del servicio"
+          />
         </Label>
         <Label className="mt-4">
           <span>Teléfono de referencia</span>
-          <Input className="mt-1" placeholder='Ingresa el teléfono de referencia'/>
+          <Input
+            className="mt-1"
+            placeholder="Ingresa el teléfono de referencia"
+          />
         </Label>
         <Label className="mt-4">
           <div className="relative text-gray-500 focus-within:text-purple-600">
-            <input
-              className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              
-            />
+            <input className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input" />
             <button className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
               Click
             </button>
@@ -42,8 +46,7 @@ function CrearServicio() {
         </Label>
       </div>
 
-
-        {/*
+      {/*
         <Label>
           <span>Invalid input</span>
           <Input className="mt-1" valid={true} placeholder="Jane Doe" />
@@ -51,7 +54,7 @@ function CrearServicio() {
         </Label>
 */}
     </Layout>
-  )
+  );
 }
 
-export default CrearServicio
+export default CrearServicio;
--- pages/administracion/cajas/editar/[id].tsx
+++ pages/administracion/cajas/editar/[id].tsx
@@ -45,6 +45,50 @@ function EditarServicio({ id }: props) {
     doFetch();
   }, []);
 
+  const updateServiceRoute = "Servicios/updateServicio/";
+  const updateUbicacionRoute = "Ubicaciones/updateUbicaciones/";
+  const updateReferencesRoute = "Referencia/UpdateReferences/";
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await fetch(`${URL.baseUrl}${updateServiceRoute}${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre: name,
+        imagenUrl: "",
+      }),
+    });
+    await fetch(`${URL.baseUrl}${updateUbicacionRoute}${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        descripcion: ubicacion,
+        imagen: "",
+        video: "",
+        serviciosId: id,
+        estado: service?.status == "success" ? true : false,
+      }),
+    });
+    await fetch(`${URL.baseUrl}${updateReferencesRoute}${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nombre: encharged,
+        numerocel: cellphone,
+        serviciosId: id,
+      }),
+    });
+    router.back();
+  };
+
   useEffect(() => {
     if (service?.name) {
       setname(service!.name);
@@ -54,13 +98,11 @@ function EditarServicio({ id }: props) {
     }
   }, [service]);
 
-  const handleAlertClick = () => {
-    setShowAlert(true);
-  };
-
   const handleAlertConfirm = () => {
-    setShowAlert(false);
-    router.back();
+    const form = document.getElementById("miFormulario") as HTMLFormElement;
+    if (form) {
+      form.submit();
+    }
   };
 
   const handleAlertCancel = () => {
@@ -81,69 +123,72 @@ function EditarServicio({ id }: props) {
         </Link>
       </div>
       <div className="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
-        <Label>
-          <span>Nombre del servicio</span>
-          <Input
-            className="mt-1"
-            placeholder="Ingresa el nombre del servicio"
-            value={name}
-            onChange={(e) => setname(e.target.value)}
-          />
-        </Label>
-        <Label className="mt-4">
-          <span>Ubicación</span>
-          <Input
-            className="mt-1"
-            placeholder="Ingresa la ubicación del servicio"
-            value={ubicacion}
-            onChange={(e) => setubicacion(e.target.value)}
-          />
-        </Label>
-        <Label className="mt-4">
-          <span>Encargado</span>
-          <Input
-            className="mt-1"
-            placeholder="Ingresa el encargado del servicio"
-            value={encharged}
-            onChange={(e) => setencharged(e.target.value)}
-          />
-        </Label>
-        <Label className="mt-4">
-          <span>Teléfono de referencia</span>
-          <Input
-            className="mt-1"
-            placeholder="Ingresa el teléfono de referencia"
-            value={cellphone}
-            onChange={(e) => setcellphone(e.target.value)}
-          />
-        </Label>
-        <Label className="mt-4">
-          <div className="relative text-gray-500 focus-within:text-purple-600">
-            <input
-              className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
-              disabled
+        <form id="miFormulario" onSubmit={handleSubmit}>
+          <Label>
+            <span>Nombre del servicio</span>
+            <Input
+              className="mt-1"
+              placeholder="Ingresa el nombre del servicio"
+              value={name}
+              onChange={(e) => setname(e.target.value)}
             />
-            <button
-              onClick={handleAlertClick}
-              className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-            >
-              Editar
-            </button>
-            {showAlert && (
-              <SweetAlert
-                warning // Puedes personalizar el tipo de alerta (success, error, warning, etc.)
-                title="Atención"
-                confirmBtnText="Confirmar"
-                cancelBtnText="Cancelar"
-                showCancel
-                onConfirm={handleAlertConfirm}
-                onCancel={handleAlertCancel}
+          </Label>
+          <Label className="mt-4">
+            <span>Ubicación</span>
+            <Input
+              className="mt-1"
+              placeholder="Ingresa la ubicación del servicio"
+              value={ubicacion}
+              onChange={(e) => setubicacion(e.target.value)}
+            />
+          </Label>
+          <Label className="mt-4">
+            <span>Encargado</span>
+            <Input
+              className="mt-1"
+              placeholder="Ingresa el encargado del servicio"
+              value={encharged}
+              onChange={(e) => setencharged(e.target.value)}
+            />
+          </Label>
+          <Label className="mt-4">
+            <span>Teléfono de referencia</span>
+            <Input
+              className="mt-1"
+              placeholder="Ingresa el teléfono de referencia"
+              value={cellphone}
+              onChange={(e) => setcellphone(e.target.value)}
+            />
+          </Label>
+          <Label className="mt-4">
+            <div className="relative text-gray-500 focus-within:text-purple-600">
+              <input
+                className="block w-full pr-20 mt-1 text-sm text-black dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray form-input"
+                disabled
+              />
+              <button
+                type={"button"}
+                onClick={() => setShowAlert(true)}
+                className="absolute inset-y-0 right-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-r-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
               >
-                Esta seguro que desea actualizar este servicio?
-              </SweetAlert>
-            )}
-          </div>
-        </Label>
+                Editar
+              </button>
+              {showAlert && (
+                <SweetAlert
+                  warning // Puedes personalizar el tipo de alerta (success, error, warning, etc.)
+                  title="Atención"
+                  confirmBtnText="Confirmar"
+                  cancelBtnText="Cancelar"
+                  showCancel
+                  onConfirm={handleAlertConfirm}
+                  onCancel={handleAlertCancel}
+                >
+                  Esta seguro que desea actualizar este servicio?
+                </SweetAlert>
+              )}
+            </div>
+          </Label>
+        </form>
       </div>
     </Layout>
   );
