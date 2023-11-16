485067e3d4d64da64f14f25f9869746d26a242ca
Jh3ri <jheremymollo.02@gmail.com>
2023-11-16 12:20:23 -0400

    Carreras y facultades CRUD

--- pages/api/apiCarrer.ts
+++
@@ -1,9 +0,0 @@
-interface IApiCarrer {
-  baseUrl: string;
-}
-const URL: IApiCarrer = {
-  baseUrl: "http://localhost:5104",
-  // baseUrl: "http://apisistemaunivalle.somee.com/api/",
-};
-
-export default URL;
--- routes/sidebar.tsx
+++ routes/sidebar.tsx
@@ -86,6 +86,10 @@ const routes: IRoute[] = [
         path: "/administracion/direccionDeCarrera/carrera",
         name: "Carreras",
       },
+      {
+        path: "/administracion/direccionDeCarrera/facultad",
+        name: "Facultades",
+      },
     ],
   },
   {
@@ -164,20 +168,19 @@ const routes: IRoute[] = [
       },
     ],
   },
-   {
+  {
     path: "/usuarios",
     icon: "PeopleIcon",
     name: "Usuarios",
     routes: [
-       {
+      {
         path: "/usuarios/listarUsuarios",
         name: "Listar Usuarios",
       },
       {
         path: "/usuarios/registrar",
         name: "Registrar",
-      }
-      
+      },
     ],
     exact: true,
   },
