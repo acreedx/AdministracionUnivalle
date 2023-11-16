05753a5b3d720f68d752c94b84dd79ddfc35df91
Adrian Herrera <Adrianhlinares@gmail.com>
2023-11-16 18:36:41 -0400

    merge

--- pages/gabineteMedico/eliminarDatos/horariosDel.tsx
+++ pages/gabineteMedico/eliminarDatos/horariosDel.tsx
@@ -33,7 +33,6 @@ import SearchBar from "components/searchBar";
 import Modal from '../../../components/modal'
 import RegistrarPage from '../registrar/index'
 
-<<<<<<< HEAD:pages/gabineteMedico/eliminarDatos/horariosDel.tsx
 interface RefereciaProps {
   pathEnable:string;
   pathDisable:string;
@@ -41,10 +40,6 @@ interface RefereciaProps {
 }
 
 const EliminarHorario: FC<RefereciaProps> = ({
-=======
-import { IDiplomados } from "utils/interfaces/PostGrado/MDD";
-import URL from "pages/api/apiCareerDirection";
->>>>>>> 46be6f7300240741cae930e923987436e13225dc:pages/administracion/postgradoDiplomados/index.tsx
 
  pathEnable,
  pathDisable,
@@ -173,9 +168,6 @@ import URL from "pages/api/apiCareerDirection";
     <div>
       {!isLoading ? (
         <>
-<<<<<<< HEAD:pages/gabineteMedico/eliminarDatos/horariosDel.tsx
-          <PageTitle>Horarios de Atención - {title}</PageTitle>
-=======
           <PageTitle>Maestrias, Doctorados y Diplomados - PostGrado</PageTitle>
 
           <div className="mb-8">
@@ -183,7 +175,6 @@ import URL from "pages/api/apiCareerDirection";
               <Button size="large">Agregar Diplomado</Button>
             </Link>
           </div>
->>>>>>> 46be6f7300240741cae930e923987436e13225dc:pages/administracion/postgradoDiplomados/index.tsx
 
           {dataTable2.length > 0 ? (
             <>
